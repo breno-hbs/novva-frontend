dec2fb40b069371ade055baec988f8d74ef0e514
Pedro Henrique <pedrosntos194@gmail.com>
2025-11-20 19:09:16 -0300

    fix: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,33 @@
 
+// import "./../styles/Navbar.css";
+// import { useContext } from "react";
+// import { ThemeContext } from "../context/ThemeContext";
+// import LogoNovva from "../assets/img/Novva_logo.png";
+
+// export default function Navbar() {
+//   const { darkMode, setDarkMode } = useContext(ThemeContext);
+
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar-logo">
+//         <img
+//           src={LogoNovva}
+//           alt="NOVVA Logo"
+//           className="logo-img"
+//         />
+//         {/* <span>NOVVA</span> */}
+//       </div>
+
+//       <div
+//         className="theme-toggle"
+//         onClick={() => setDarkMode(!darkMode)}
+//       >
+//         {darkMode ? "🌙" : "☀️"}
+//       </div>
+//     </nav>
+//   );
+// }
+
 import "./../styles/Navbar.css";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
@@ -8,14 +37,13 @@ export default function Navbar() {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${darkMode ? "dark" : ""}`}>
       <div className="navbar-logo">
         <img
           src={LogoNovva}
           alt="NOVVA Logo"
           className="logo-img"
         />
-        {/* <span>NOVVA</span> */}
       </div>
 
       <div
